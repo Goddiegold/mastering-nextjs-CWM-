33df0accd06d883ba08fa41f13a3d32db4633542
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2024-09-16 09:48:06 +0100

    SEO optimization with meta data objects.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import Image from "next/image";
 import backgroundImage from "@/public/images/backgorund.jpg";
+import { Metadata } from "next";
 
 // export default async function Home() {
 //   const session = await getServerSession(authOptions)
@@ -18,7 +19,7 @@ import backgroundImage from "@/public/images/backgorund.jpg";
 export default async function Home() {
   return (
     <main className="relative h-screen">
-      <Image
+      {/* <Image
         src={backgroundImage}
         alt="background iamge"
         fill
@@ -27,7 +28,23 @@ export default async function Home() {
         sizes="(max-width:480px) 100vw, (max-width:768px) 50vw, 33vw"
         quality={100}
         priority
-      />
+      /> */}
+      <h1 className="font-inter">hello world</h1>
     </main>
   );
+}
+
+//for staic content
+// export const metadata:Metadata = {
+//   title:"..."
+// }
+
+//for dynamic content
+export async function generateMetadata(): Promise<Metadata> {
+  const product = await fetch("/api/products")
+
+  return {
+    title: "[product.title]",
+    description: "[product.details]"
+  }
 }
